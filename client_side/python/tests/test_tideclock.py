01d73454d7e309eaca4354ca0644f18ead84adda
Brian Boucher <bboucher@maxeler.com>
2019-09-15 14:49:05 -0400

    Updating client_side/python

--- client_side/python/tests/test_tideclock.py
+++ client_side/python/tests/test_tideclock.py
@@ -11,12 +11,15 @@ def test_status():
     pprint(status_json)
     FIELDS = ['current_time',
               'dac_values',
+              'displayed_time',
+              'motion_enabled',
               'display_enabled',
               'display_forced',
               'display_mode',
               'main_bus',
               'neopixel_colors',
-              'position']
+              'position',
+              'avg_loop_time']
     for fld in FIELDS:
         assert fld in status_json
     for string_fld in ['current_time', 'display_mode']:
@@ -24,16 +27,19 @@ def test_status():
     for list_fld in ['dac_values', 'neopixel_colors']:
         assert isinstance(status_json[list_fld], list)
         assert len(status_json[list_fld]) == 24
-    for bool_fld in ['display_enabled', 'display_forced']:
+    for bool_fld in ['motion_enabled', 'display_enabled', 'display_forced']:
         assert isinstance(status_json[bool_fld], bool)
-    assert isinstance(status_json['position'], int)
-    assert isinstance(status_json['main_bus'], float)
+    for int_fld in ['position', 'avg_loop_time']:
+        assert isinstance(status_json[int_fld], int)
+    for float_fld in ['main_bus', 'displayed_time']:
+        assert isinstance(status_json[float_fld], float)
 
 
 def test_config():
     config_json = tideclock.get_config()
     pprint(tideclock.get_config())
-    for fld in ['time_0_pos', 'time_0_pixel', 'brightness_day', 'brightness_night', 'dac_calibrations']:
+    for fld in ['time_0_pos', 'time_0_pixel', 'pixel_dir',
+                'brightness_day', 'brightness_night', 'dac_calibrations']:
         assert (fld in config_json)
         if fld != 'dac_calibrations':
             assert isinstance(config_json[fld], int)
@@ -42,13 +48,86 @@ def test_config():
             assert len(config_json[fld]) == 24
 
 
+def test_motion_enable():
+    assert tideclock.disable_motion() == 'Motion disabled'
+    assert not tideclock.get_status()['motion_enabled']
+    assert tideclock.enable_motion() == 'Motion enabled'
+    assert tideclock.get_status()['motion_enabled']
+
+
 def test_seek():
+    assert tideclock.disable_motion() == 'Motion disabled'
     assert tideclock.seek(50, True) == 50
+    assert tideclock.get_status()['position'] == 50
     assert tideclock.seek(20, False) == 70
+    assert tideclock.get_status()['position'] == 70
+    time_pos = tideclock.seek_time(5.0)
+    status_json = tideclock.get_status()
+    assert status_json['position'] == time_pos
+    assert 4.995 < status_json['displayed_time'] < 5.005
+    assert tideclock.enable_motion() == 'Motion enabled'
 
 
 def test_time_zero_position():
+    assert tideclock.disable_motion() == 'Motion disabled'
     assert tideclock.seek(100, True) == 100
     assert tideclock.set_time_zero_position() == 100
     assert tideclock.seek(200, True) == 200
     assert tideclock.get_time_zero_position() == 100
+    assert tideclock.enable_motion() == 'Motion enabled'
+
+
+def test_set_dac():
+    assert tideclock.set_dac(0, 100) == 100
+
+
+def test_dac_calibrate():
+    assert tideclock.disable_display() == 'Display disabled'
+    assert tideclock.set_dac(3, 150) == 150
+    assert tideclock.set_dac_calibrate(3, 0) == 150
+    assert tideclock.set_dac(3, 200) == 200
+    assert tideclock.get_dac_calibrate(3, 0) == 150
+    assert tideclock.enable_display() == 'Display enabled'
+
+
+def test_neopixel_brightness():
+    assert tideclock.set_neopixel_brightness(True, 10) == 10
+    assert tideclock.set_neopixel_brightness(False, 20) == 20
+    config_json = tideclock.get_config()
+    assert config_json['brightness_day'] == 10
+    assert config_json['brightness_night'] == 20
+
+
+def test_neopixel_offset():
+    assert tideclock.set_neopixel_offset(3) == 3
+    assert tideclock.get_neopixel_offset() == 3
+    assert tideclock.get_config()['time_0_pixel'] == 3
+    assert tideclock.set_neopixel_offset(24) == 0
+    assert tideclock.get_neopixel_offset() == 0
+    assert tideclock.get_config()['time_0_pixel'] == 0
+
+
+def test_neopixel_direction():
+    assert tideclock.set_neopixel_direction(-3) == -1
+    assert tideclock.get_neopixel_direction() == -1
+    assert tideclock.get_config()['pixel_dir'] == -1
+    assert tideclock.set_neopixel_direction(3) == 1
+    assert tideclock.get_neopixel_direction() == 1
+    assert tideclock.get_config()['pixel_dir'] == 1
+
+
+def test_display_enable():
+    assert tideclock.disable_display() == 'Display disabled'
+    assert not tideclock.get_status()['display_enabled']
+    assert tideclock.enable_display() == 'Display enabled'
+    assert tideclock.get_status()['display_enabled']
+
+
+def test_force_display():
+    for mode in ['NORMAL', 'DEPOISON', 'WAVE', 'ALTERNATING', 'RANDOM', 'PULSE', 'DEMO']:
+        assert tideclock.force_display(mode) == 'Setting display mode to ' + mode
+        status_json = tideclock.get_status()
+        assert status_json['display_forced']
+        assert status_json['display_mode'] == mode
+    assert tideclock.release_display() == 'Display released'
+    assert not tideclock.get_status()['display_forced']
--- client_side/python/tideclock.py
+++ client_side/python/tideclock.py
@@ -8,8 +8,8 @@ def get_text_endpoint(name: str) -> str:
     return requests.get(URL + name).text
 
 
-def post_text_endpoint(name: str) -> str:
-    return requests.post(URL + name).text
+def post_text_endpoint(name: str, data: str = "") -> str:
+    return requests.post(URL + name, data=data).text
 
 
 def get_tides() -> str:
@@ -33,8 +33,20 @@ def save_config() -> dict:
 
 
 def seek(pos: int, abs: bool = True) -> int:
-    ep = URL + 'seek/' + ('abs' if abs else 'rel')
-    return int(requests.post(ep, data=str(pos)).text)
+    ep = 'seek/' + ('abs' if abs else 'rel')
+    return int(post_text_endpoint(ep, str(pos)))
+
+
+def seek_time(hr: float) -> int:
+    return int(post_text_endpoint('seek/time', str(hr)))
+
+
+def enable_motion() -> str:
+    return post_text_endpoint('motion/enable')
+
+
+def disable_motion() -> str:
+    return post_text_endpoint('motion/disable')
 
 
 def get_time_zero_position() -> int:
@@ -43,3 +55,63 @@ def get_time_zero_position() -> int:
 
 def set_time_zero_position() -> int:
     return int(post_text_endpoint('time_zero'))
+
+
+def parse_dac_set(response: str) -> int:
+    return int(response[response.index('>') + 2:], 16)
+
+
+def set_dac(num: int, value: int) -> int:
+    headers = dict(num=str(num), value=str(value))
+    response = requests.post(URL + 'dac', headers=headers).text
+    return parse_dac_set(response)
+
+
+def get_dac_calibrate(num: int, pt: int) -> int:
+    headers = dict(num=str(num), pt=str(pt))
+    response = requests.get(URL + 'dac/calibrate', headers=headers).text
+    return parse_dac_set(response)
+
+
+def set_dac_calibrate(num: int, pt: int) -> int:
+    headers = dict(num=str(num), pt=str(pt))
+    response = requests.post(URL + 'dac/calibrate', headers=headers).text
+    return parse_dac_set(response)
+
+
+def set_neopixel_brightness(is_day: bool, brightness: int) -> int:
+    headers = dict(time=('day' if is_day else 'night'), value=str(brightness))
+    response = requests.post(URL + 'neopixel/brightness', headers=headers).text
+    return parse_dac_set(response)
+
+
+def get_neopixel_offset() -> int:
+    return int(get_text_endpoint('neopixel/offset'))
+
+
+def set_neopixel_offset(offset: int) -> int:
+    return int(post_text_endpoint('neopixel/offset', str(offset)))
+
+
+def get_neopixel_direction() -> int:
+    return int(get_text_endpoint('neopixel/direction'))
+
+
+def set_neopixel_direction(dir: int) -> int:
+    return int(post_text_endpoint('neopixel/direction', str(dir)))
+
+
+def disable_display() -> str:
+    return post_text_endpoint('display/disable')
+
+
+def enable_display() -> str:
+    return post_text_endpoint('display/enable')
+
+
+def release_display() -> str:
+    return post_text_endpoint('display/release')
+
+
+def force_display(mode: str) -> str:
+    return post_text_endpoint('display/' + mode.lower())
